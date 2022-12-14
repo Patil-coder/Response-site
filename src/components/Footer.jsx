5c4ea4146670b8f908b08aef8dc3aec5de20e9fb
patil-coder <pnpatil490@gmail.com>
2022-12-14 05:26:48 -0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer>
+    <div>
+    <h1>Techystar</h1>
+    <p>@all right reserved</p>
+    </div>
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href='https://youtube.com/@praveenpatil6147' target={"blank"}>Youtube</a>
+            <a href='https://instagram.com/pnpatil490' target={"blank"}>Instagram</a>
+            <a href='https://github.com/Patil-coder' target={"blank"}>GitHub</a>
+        </div>
+    </div>
+   </footer>
+  )
+}
+
+export default Footer
